726a81ab265f8167fba0825a4a592a2b94be194f
Andres Gomez <learningxandres@gmail.com>
2021-10-22 07:09:17 -0500

    Se desarrolla, la ruta Error con su respectivo componente

---
+++ src/components/Error.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="error_container">
+        <h1>Error Error Error Error Error Error Error</h1>
+        <img src="https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/styles/image_750x424/public/field/image/article/rick-morty-pelicula.jpg?itok=thJFYqHN" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.error_container{
+    width: 50%;
+    margin: auto;
+}
+.error_container h1{
+    color: white;
+    text-decoration: line-through;
+    font-weight: bold;
+}
+.error_container img{
+    width: 100%;
+}
+</style>
